9200c2ce8fd14a4632e82cf00f26f6abcb6d1205
BABY-MUSIC <tridevk442@gmail.com>
2024-08-11 01:37:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/BABY-MUSIC/YTMUSIC",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
