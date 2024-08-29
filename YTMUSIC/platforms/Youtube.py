2209ecde860b1400213342e97d861c8825e807f6
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-08-29 12:41:30 +0530

    Update Youtube.py

--- YTMUSIC/platforms/Youtube.py
+++ YTMUSIC/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from SONALI.utils.database import is_on_off
-from SONALI.utils.formatters import time_to_seconds
+from YTMUSIC.utils.database import is_on_off
+from YTMUSIC.utils.formatters import time_to_seconds
 
 async def shell_cmd(cmd):
     proc = await asyncio.create_subprocess_shell(
