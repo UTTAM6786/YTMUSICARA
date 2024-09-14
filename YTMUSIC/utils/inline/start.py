cec45f7f55cab614b43be7f91cfbb50f2e1f5925
UTTAM RATHORE <uk977061@gmail.com>
2024-09-14 10:29:07 +0530

    Update start.py

--- YTMUSIC/utils/inline/start.py
+++ YTMUSIC/utils/inline/start.py
@@ -7,38 +7,38 @@ from config import SUPPORT_CHAT, SUPPORT_CHANNEL, OWNER_ID
 def start_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(text="• sᴇᴛᴛɪɴɢ •", callback_data="settings_helper"),
+            InlineKeyboardButton(text="˹ sᴇᴛᴛɪɴɢ ˼", callback_data="settings_helper"),
         ],
         [
-            InlineKeyboardButton(text="• sᴜᴘᴘᴏꝛᴛ •", url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text="˹ sᴜᴘᴘᴏꝛᴛ ˼", url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
     
 def private_panel(_):
     buttons = [
-        [InlineKeyboardButton(text="• ʜᴏᴡ ᴛᴏ ᴜsᴇ? ᴄᴏᴍᴍᴀɴᴅ ᴍᴇɴᴜ •", callback_data="settings_back_helper")]
+        [InlineKeyboardButton(text="⌯ ʜᴏᴡ ᴛᴏ ᴜsᴇ? ᴄᴏᴍᴍᴀɴᴅ ᴍᴇɴᴜ ⌯", callback_data="settings_back_helper")]
     ]
     if SUPPORT_CHANNEL and SUPPORT_CHAT:
         buttons.append(
             [
-                InlineKeyboardButton(text="• ᴄʜᴀɴɴᴇʟ •", url=f"{SUPPORT_CHANNEL}"),
-                InlineKeyboardButton(text="• sᴜᴘᴘᴏꝛᴛ •", url=f"{SUPPORT_CHAT}"),
+                InlineKeyboardButton(text="˹ ᴄʜᴀɴɴᴇʟ ˼", url=f"{SUPPORT_CHANNEL}"),
+                InlineKeyboardButton(text="˹ sᴜᴘᴘᴏꝛᴛ ˼", url=f"{SUPPORT_CHAT}"),
             ]
         )
     else:
         if SUPPORT_CHANNEL:
             buttons.append(
-                [InlineKeyboardButton(text="• ᴄʜᴀɴɴᴇʟ •", url=f"{SUPPORT_CHANNEL}")]
+                [InlineKeyboardButton(text="˹ ᴄʜᴀɴɴᴇʟ ˼", url=f"{SUPPORT_CHANNEL}")]
             )
         if SUPPORT_CHAT:
             buttons.append(
-                [InlineKeyboardButton(text="• sᴜᴘᴘᴏꝛᴛ •", url=f"{SUPPORT_CHAT}")]
+                [InlineKeyboardButton(text="˹ sᴜᴘᴘᴏꝛᴛ ˼", url=f"{SUPPORT_CHAT}")]
             )
     buttons.append(
         [
             InlineKeyboardButton(
-                text= "• ᴧᴅᴅ мᴇ ʙᴧʙʏ •",
+                text= "⌯ ᴧᴅᴅ мᴇ ʙᴧʙʏ ⌯",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ]
