16bbc6aa68931936425658a09c26204349bad7a8
UTTAM RATHORE <uk977061@gmail.com>
2024-09-23 15:43:33 +0530

    Update start.py

--- YTMUSIC/utils/inline/start.py
+++ YTMUSIC/utils/inline/start.py
@@ -1,36 +1,34 @@
 from pyrogram.types import InlineKeyboardButton
+
 import config
 from YTMUSIC import app
-from config import SUPPORT_CHAT, SUPPORT_CHANNEL, OWNER_ID
+
 
 def start_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(text="• sᴇᴛᴛɪɴɢ •", callback_data="settings_helper"),
-        ],
-        [
-            InlineKeyboardButton(text="• sᴜᴘᴘᴏꝛᴛ •", url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
-    
-def private_panel(is_owner):
-    buttons = []
-    
-    buttons.append(
-        [InlineKeyboardButton(text="❖ ᴛᴧᴘ тᴏ sᴇᴇ ᴍᴧɢɪᴄ ❖", url=f"https://t.me/{app.username}?startgroup=true")]
-    )
-    
-    if is_owner:
-        buttons.append(
-            [
-                InlineKeyboardButton(text="˹ ❍ᴡɴᴇꝛ ˼", user_id=OWNER_ID),
-                InlineKeyboardButton(text="˹ ᴍᴏᴅᴇ ˼", callback_data="ubot_cb"),
-            ]
-        )
-    else:
-        buttons.append(
-            [InlineKeyboardButton(text="˹ ᴧʙᴏᴜᴛ ˼", callback_data=f"abot_cb")]
-        )
-        
+
+
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_12"], callback_data=f"modebot_cb"),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], callback_data=f"abot_cb"),
+        ],
+    ]
     return buttons
