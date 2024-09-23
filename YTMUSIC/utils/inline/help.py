2f4ed5ae276b650655e636797a680f1c317335e8
UTTAM RATHORE <uk977061@gmail.com>
2024-09-23 16:58:09 +0530

    Update help.py

--- YTMUSIC/utils/inline/help.py
+++ YTMUSIC/utils/inline/help.py
@@ -49,7 +49,7 @@ def help_back_markup(_):
             [
                 InlineKeyboardButton(
                     text="↺ ʙᴧᴄᴋ ↻",
-                    callback_data=f"ubot_cb",
+                    callback_data=f"mbot_cb",
                 ),
                 InlineKeyboardButton(text="• ᴄʟᴏsᴇ •", callback_data=f"close"),
             ],
