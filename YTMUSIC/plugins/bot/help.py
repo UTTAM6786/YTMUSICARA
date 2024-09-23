2d90018c828ebaea63c375a2c51ea3552f0736c8
UTTAM RATHORE <uk977061@gmail.com>
2024-09-23 17:08:50 +0530

    Update help.py

--- YTMUSIC/plugins/bot/help.py
+++ YTMUSIC/plugins/bot/help.py
@@ -15,7 +15,7 @@ from YTMUSIC.utils.stuffs.buttons import BUTTONS
 from YTMUSIC.utils.stuffs.helper import Helper
 
 @app.on_message(filters.command(["help"]) & filters.private & ~BANNED_USERS)
-@app.on_callback_query(filters.regex("mbot_cb") & ~BANNED_USERS)
+@app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
 async def helper_private(
     client: app, update: Union[types.Message, types.CallbackQuery]
 ):
