cd7ff1cee2950759e4a94cf18b450c35ee20f681
UTTAM RATHORE <uk977061@gmail.com>
2024-09-23 16:52:24 +0530

    Update help.py

--- YTMUSIC/plugins/bot/help.py
+++ YTMUSIC/plugins/bot/help.py
@@ -61,6 +61,10 @@ async def helper_cb(client, CallbackQuery):
 async def helper_cb(client, CallbackQuery):
     await CallbackQuery.edit_message_text(Helper.HELP_B, reply_markup=InlineKeyboardMarkup(BUTTONS.UBUTTON))
 
+@app.on_callback_query(filters.regex("tbot_cb") & ~BANNED_USERS)
+async def helper_cb(client, CallbackQuery):
+    await CallbackQuery.edit_message_text(Helper.HELP_B, reply_markup=InlineKeyboardMarkup(BUTTONS.TBUTTON))
+
 @app.on_callback_query(filters.regex("help_callback") & ~BANNED_USERS)
 @languageCB
 async def helper_cb(client, CallbackQuery, _):
