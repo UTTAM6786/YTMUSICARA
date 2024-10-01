db81601b8217d392555093bbb96f935177687b12
UTTAM RATHORE <uk977061@gmail.com>
2024-10-01 12:51:46 +0530

    Update exceptions.py

--- YTMUSIC/utils/exceptions.py
+++ YTMUSIC/utils/exceptions.py
@@ -1,3 +1,9 @@
 class AssistantErr(Exception):
     def __init__(self, errr: str):
         super().__init__(errr)
+        
+class DownloadError(Exception):
+    """Custom exception for download failures."""
+
+    def __init__(self, errr: str):
+        super().__init__(errr)
