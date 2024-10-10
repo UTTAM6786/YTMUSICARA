152513d5bab5fead8829a3a56ac237ee328080fb
UTTAM RATHORE <uk977061@gmail.com>
2024-10-10 09:31:13 +0530

    Update start.py

--- YTMUSIC/plugins/bot/start.py
+++ YTMUSIC/plugins/bot/start.py
@@ -1,16 +1,16 @@
 import time
 import random
+import asyncio
 from pyrogram import filters
 from pyrogram.errors import ChannelInvalid
-from YTMUSIC.plugins.play.playlist import del_plist_msg
 from pyrogram.enums import ChatType, ChatMembersFilter
-
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
 from YTMUSIC import app
 from YTMUSIC.misc import _boot_
+from YTMUSIC.plugins.play.playlist import del_plist_msg
 from YTMUSIC.plugins.sudo.sudoers import sudoers_list
 from YTMUSIC.utils.database import (
     add_served_chat,
@@ -28,19 +28,11 @@ from config import BANNED_USERS
 from strings import get_string
 
 
-
-UTTAM = [
-    "CAACAgQAAxkBAAEBkjVnBO53WGOSXzfJ8QwxRNEkqWZiWgACfgwAAiyOaFJpsWOnSgABrAgeBA",
-    "CAACAgQAAxkBAAEBkjVnBO53WGOSXzfJ8QwxRNEkqWZiWgACfgwAAiyOaFJpsWOnSgABrAgeBA",
-    "CAACAgQAAxkBAAEBkjVnBO53WGOSXzfJ8QwxRNEkqWZiWgACfgwAAiyOaFJpsWOnSgABrAgeBA",
-    "CAACAgQAAxkBAAEBkjVnBO53WGOSXzfJ8QwxRNEkqWZiWgACfgwAAiyOaFJpsWOnSgABrAgeBA",
-        ]
-
-async def delete_sticker_after_delay(message, delay):
+async def delete_message_after_delay(message, delay):
     await asyncio.sleep(delay)
     await message.delete()
-        
-        
+
+
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
@@ -51,45 +43,43 @@ async def start_pm(client, message: Message, _):
         if name[0:3] == "del":
             await del_plist_msg(client=client, message=message, _=_)
 
-        if name[0:4] == "help":
+        elif name[0:4] == "help":
             keyboard = help_pannel(_)
-            sticker_message = await message.reply_sticker(sticker=random.choice(UTTAM))
-            asyncio.create_task(delete_sticker_after_delay(sticker_message, 5))
             return await message.reply_photo(photo=config.START_IMG_URL,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
-        if name[:8] == "connect_":
+
+        elif name[:8] == "connect_":
             chat_id = name[8:]
             try:
                 title = (await app.get_chat(chat_id)).title
             except ChannelInvalid:
-                return await message.reply_text(f"ʟᴏᴏʟ ʟɪᴋᴇ ɪ ᴀᴍ ɴᴏᴛ ᴀɴ ᴀᴅᴍɪɴ ᴏғ ᴛʜᴇ ᴄʜᴀᴛ ɪᴅ {chat_id}")
+                return await message.reply_text(f"Looks like I am not an admin of the chat ID {chat_id}")
             
-            admin_ids = [ member.user.id async for member in app.get_chat_members(chat_id, filter=ChatMembersFilter.ADMINISTRATORS)]
+            admin_ids = [member.user.id async for member in app.get_chat_members(chat_id, filter=ChatMembersFilter.ADMINISTRATORS)]
             if message.from_user.id not in admin_ids:
-                return await message.reply_text(f"sᴏʀʀʏ sɪʀ ʙᴜᴛ ɪ ᴛʜɪɴᴋ ᴛʜᴀᴛ ʏᴏᴜ ɴᴏᴛ ᴀɴ ᴀᴅᴍɪɴ ᴏғ {title} ")
+                return await message.reply_text(f"Sorry, but it seems you are not an admin of {title}.")
             a = await connect_to_chat(message.from_user.id, chat_id)
             if a:
-                await message.reply_text(f"ʏᴏᴜ ᴀʀᴇ ɴᴏᴡ ᴄᴏɴɴᴇᴄᴛᴇᴅ ᴛᴏ {title}")
+                await message.reply_text(f"You are now connected to {title}.")
             else:
                 await message.reply_text(a)
 
-        
-        if name[0:3] == "sud":
+        elif name[0:3] == "sud":
             await sudoers_list(client=client, message=message, _=_)
             if await is_on_off(2):
                 return await app.send_message(
                     chat_id=config.LOGGER_ID,
-                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                    text=f"{message.from_user.mention} started the bot to check <b>Sudo List</b>.\n\n<b>User ID:</b> <code>{message.from_user.id}</code>\n<b>Username:</b> @{message.from_user.username}",
                 )
-            return
-        if name[0:3] == "inf":
+
+        elif name[0:3] == "inf":
             m = await message.reply_text("🔎")
             query = (str(name)).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
-            results = VideosSearch(query, limit=1)
-            for result in (await results.next())["result"]:
+            results = await VideosSearch(query, limit=1).next()
+            for result in results["result"]:
                 title = result["title"]
                 duration = result["duration"]
                 views = result["viewCount"]["short"]
@@ -98,6 +88,7 @@ async def start_pm(client, message: Message, _):
                 channel = result["channel"]["name"]
                 link = result["link"]
                 published = result["publishedTime"]
+
             searched_text = _["start_6"].format(
                 title, duration, views, published, channellink, channel, app.mention
             )
@@ -119,35 +110,35 @@ async def start_pm(client, message: Message, _):
             if await is_on_off(2):
                 return await app.send_message(
                     chat_id=config.LOGGER_ID,
-                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                    text=f"{message.from_user.mention} started the bot to check <b>Track Information</b>.\n\n<b>User ID:</b> <code>{message.from_user.id}</code>\n<b>Username:</b> @{message.from_user.username}",
                 )
     else:
         out = private_panel(_)
-        baby = await message.reply_text(f"**■□□□□□□□□□ 10%**")
-        await baby.edit_text(f"**■■□□□□□□□□ 30%**")
-        await baby.edit_text(f"**■■■□□□□□□□ 30%**")
-        await baby.edit_text(f"**■■■■□□□□□□ 30%**")
-        await baby.edit_text(f"**■■■■■□□□□□ 50%**")
-        await baby.edit_text(f"**■■■■■■□□□□ 30%**")
-        await baby.edit_text(f"**■■■■■■■□□□ 70%**")
-        await baby.edit_text(f"**■■■■■■■■□□ 30%**")
-        await baby.edit_text(f"**■■■■■■■■■□ 90%**")
-        await baby.edit_text(f"**■■■■■■■■■■ 100%**")
-        await baby.edit_text(f"**𝗟𝗲𝘁'𝘀 𝗚𝗼 𝗕𝗮𝗯𝘆..🌸**")
+        baby = await message.reply_text(f"**▒▒▒▒▒▒▒▒▒▒ 0%**")
+        await baby.edit_text(f"**█▒▒▒▒▒▒▒▒▒ 10%**")
+        await baby.edit_text(f"**██▒▒▒▒▒▒▒▒ 20%**")
+        await baby.edit_text(f"**███▒▒▒▒▒▒▒ 30%**")
+        await baby.edit_text(f"**████▒▒▒▒▒▒ 40%**")
+        await baby.edit_text(f"**█████▒▒▒▒▒ 50%**")
+        await baby.edit_text(f"**██████▒▒▒▒ 60%**")
+        await baby.edit_text(f"**███████▒▒▒ 70%**")
+        await baby.edit_text(f"**████████▒▒ 80%**")
+        await baby.edit_text(f"**█████████▒ 90%**")
+        await baby.edit_text(f"**██████████ 100%**")
+        await baby.edit_text(f"**❖ ɴᴏᴡ sᴛᴀʀᴛᴇᴅ..**")
         await baby.delete()
-        
-        sticker_message = await message.reply_sticker(sticker=random.choice(UTTAM))
-        asyncio.create_task(delete_sticker_after_delay(sticker_message, 5))
+
         await message.reply_photo(photo=config.START_IMG_URL,
-        caption=_["start_2"].format(message.from_user.mention, app.mention),
-        reply_markup=InlineKeyboardMarkup(out),
+            caption=_["start_2"].format(message.from_user.mention, app.mention),
+            reply_markup=InlineKeyboardMarkup(out),
         )
         if await is_on_off(2):
             return await app.send_message(
                 chat_id=config.LOGGER_ID,
-                text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                text=f"{message.from_user.mention} started the bot.\n\n<b>User ID:</b> <code>{message.from_user.id}</code>\n<b>Username:</b> @{message.from_user.username}",
             )
-        
+
+
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
 @LanguageStart
 async def start_gp(client, message: Message, _):
